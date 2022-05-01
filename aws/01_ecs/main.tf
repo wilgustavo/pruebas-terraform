97f0897d0a1fda73a41ade319e083bc44a040455
Gustavo Wilgenhoff <gustavo.wilgenhoff@lamercantil.com.ar>
2022-05-01 13:15:40 -0300

    aws_security_group

--- aws/01_ecs/main.tf
+++ aws/01_ecs/main.tf
@@ -18,10 +18,36 @@ data "aws_ami" "ubuntu" {
 
     filter {
         name   = "virtualization-type"
-        values = ["hvs"]
+        values = ["hvm"]    
     }
 
     owners = ["099720109477"]
+}
+
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "Allow SSH inbound traffic"
+    vpc_id = var.vpc_id
+    
+    ingress {
+        description = "SSH from VPC"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_ssh"
+    }
+  
 }
 
 output "name" {
